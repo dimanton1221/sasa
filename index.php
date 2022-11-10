bf285fde498fb75b9e5dc9862228eeedfb876136
darmanti <116934032+dimanton1221@users.noreply.github.com>
2022-11-10 09:50:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 ini_set('post_max_size', '640000M');
 ini_set('upload_max_filesize', '600004M');
-phpinfo();
+$data = file_get_contents("/workspace/.heroku/php/etc/php/php.ini");
 
-?>
-<?php echo php_ini_loaded_file(); ?>
+echo $data;
